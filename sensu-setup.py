2ec0b97b0e7ec8a3f1409b907db516f3ea2e674c
Philip Bove <pgbson@gmail.com>
2016-07-06 22:00:56 -0400

    fixed some things?

--- sensu-setup.py
+++ sensu-setup.py
@@ -1,3 +1,4 @@
+
 #Note: This is to be launched from an ansible deployment playbook 
 #TODO: when deploying we need another script the uses scp to copy the keys to clients
 #TODO: install redis,sensu,rabbitmq,uchiwa start and enable at end of script
@@ -7,21 +8,21 @@ import subprocess as sp
 
 #install rabbimq and redis from ansible
 #might have to pull these two into a second script    
-sp.call(["wget -q http://sensu.global.ssl.fastly.net/apt/pubkey.gpg -O- | sudo apt-key add -"])
-sp.call(['echo "deb     http://sensu.global.ssl.fastly.net/apt sensu main" | sudo tee /etc/apt/sources.list.d/sensu.list'])
+sp.call(["wget -q http://sensu.global.ssl.fastly.net/apt/pubkey.gpg -O- | apt-key add -"], shell = True)
+sp.call(['echo "deb     http://sensu.global.ssl.fastly.net/apt sensu main" | tee /etc/apt/sources.list.d/sensu.list'], shell = True)
 #sensu package is installed with built in ansible commmand'
 sp.call(["mkdir /home/server/ssl"])
-sp.call(["cd /home/server/ssl && wget http://sensuapp.org/docs/0.25/files/sensu_ssl_tool.tar && tar -xvf sensu_ssl_tool.tar"])
+sp.call(["cd /home/server/ssl && wget http://sensuapp.org/docs/0.25/files/sensu_ssl_tool.tar && tar -xvf sensu_ssl_tool.tar"], shell = True)
 sp.call(["cd /home/server/ssl/sensu_ssl_tool $$ ./ssl_certs.sh generat"])
 sp.call("mkdir /etc/sensu/ssl")
-sp.call(["cp /home/server/ssl/sensu_ssl_tool/client /home/server/ssl/sensu_ssl_tool/sensu_ca /home/server/ssl/sensu_ssl_tool/server /etc/sensu/ssl"])
-sp.call(["cp /home/server/sensu_ssl_tool/sensu_ca/cacert.pem /home/server/sensu_ssl_tool/server/cert.pem /home/server/sensu_ssl_tool/server/key.pem /etc/rabbitmq/ssl"])
+sp.call(["cp /home/server/ssl/sensu_ssl_tool/client /home/server/ssl/sensu_ssl_tool/sensu_ca /home/server/ssl/sensu_ssl_tool/server /etc/sensu/ssl"], shell = True)
+sp.call(["cp /home/server/sensu_ssl_tool/sensu_ca/cacert.pem /home/server/sensu_ssl_tool/server/cert.pem /home/server/sensu_ssl_tool/server/key.pem /etc/rabbitmq/ssl"], shell = True)
 #config rabbitmq
-sp.call(["rabbitmqtcl add_vhost /sensu"])
-sp.call(["rabbitmqtcl add_user sensu password"])
-sp.call(['rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"'])
+sp.call(["rabbitmqtcl add_vhost /sensu"], shell = True)
+sp.call(["rabbitmqtcl add_user sensu password"], shell = True)
+sp.call(['rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"'], shell = True)
 #throw certs into etc/sensu/etc
-sp.call(["cp /home/server/sensu_ssl_tool/server/cert.pem  /home/server/sensu_ssl_tool/server/key.pem /etc/sensu/ssl"])
+sp.call(["cp /home/server/sensu_ssl_tool/server/cert.pem  /home/server/sensu_ssl_tool/server/key.pem /etc/sensu/ssl"], shell = True)
 #Asnible will add the config files to the servers
 #rabbitmq: /etc/sensu/conf.d/rabbitmq.json
 #api: /etc/sensu/conf.d/api.json
