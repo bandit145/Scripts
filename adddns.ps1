f6b9411ee925e9379abbcbe042b3214371e8555e
Philip Bove <pgbson@gmail.com>
2016-07-28 01:11:28 -0400

    Added error handling (need to add specific error handling)

--- adddns.ps1
+++ adddns.ps1
@@ -2,10 +2,15 @@
 #picked up by windows domain controllers 
 $user = Read-Host 'Domain admin name'
 $session = New-PSSession -ComputerName dc1 -Credential meme\$user
-Invoke-Command -Session $session -ScriptBlock {
-	$ipaddress = Read-Host 'What IP address are you adding to dns? '
-	$computername = Read-Host 'What will the computer address be? '
-	Get-DhcpServerv4Lease -IPAddress $ipaddress -ComputerName dc1.meme.com | Add-DhcpServerv4Reservation
-	Add-DnsServerResourceRecordA -zonename meme.com -name $computername -ipv4address $ipaddress
-	Exit-PSSession
+try{
+	Invoke-Command -Session $session -ScriptBlock {
+		$ipaddress = Read-Host 'What IP address are you adding to dns? '
+		$computername = Read-Host 'What will the computer address be? '
+		Get-DhcpServerv4Lease -IPAddress $ipaddress -ComputerName dc1.meme.com | Add-DhcpServerv4Reservation
+		Add-DnsServerResourceRecordA -zonename meme.com -name $computername -ipv4address $ipaddress
+		Exit-PSSession
+		}
+}
+catch{
+	Write-Host "Some of your Information is incorrect"
 }
