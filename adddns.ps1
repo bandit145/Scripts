d54804e31290af77c347950fa19350f98bffca2f
Philip Bove <pgbson@gmail.com>
2016-08-02 01:07:04 -0400

    added paramters to adddns per per practice, going to be moving to a  function/ps module

--- adddns.ps1
+++ adddns.ps1
@@ -1,11 +1,14 @@
 #This takes a DHCP lease and makes it a reservation, This is mostly for linux servers which do not have their hostnames automatically
 #picked up by windows domain controllers 
-$user = Read-Host 'Domain admin name'
-$session = New-PSSession -ComputerName dc1 -Credential meme\$user
+param (	[Parameter(Mandatory=$True)]
+		[string]$admin,
+		[Parameter(Mandatory=$True)]
+		[string]$ipaddress,
+		[Parameter(Mandatory=$True)]
+		[string]$computername)
+$session = New-PSSession -ComputerName dc1 -Credential meme\$admin
 try{
 	Invoke-Command -Session $session -ScriptBlock {
-		$ipaddress = Read-Host 'What IP address are you adding to dns? '
-		$computername = Read-Host 'What will the computer address be? '
 		Get-DhcpServerv4Lease -IPAddress $ipaddress -ComputerName dc1.meme.com | Add-DhcpServerv4Reservation
 		Add-DnsServerResourceRecordA -zonename meme.com -name $computername -ipv4address $ipaddress
 		Exit-PSSession
