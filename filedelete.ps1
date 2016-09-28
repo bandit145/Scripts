d1655bdc6fa39cf5ae8af3f5d824bdfaef86a333
Philip Bove <pgbson@gmail.com>
2016-09-28 15:33:22 -0400

    fixed script

--- filedelete.ps1
+++ filedelete.ps1
@@ -1,13 +1,13 @@
 #Written by Philip Bove
 #9/28/2016
 #
-$files = Get-ChildItem userprofiles #Put path to folder you would like to run against
+$files = Get-ChildItem E:/downloads #Put path to folder you would like to run against
 $date = Get-Date
-$date = $date.AddDays(-10)
+$date = $date.AddDays(-1)
 foreach($file in $files){
-	$fileinfo = Get-Item $file
+	$fileinfo = Get-Item $file.PSPath
 	if($file.CreationTime -le $date){
-		Remove-Item -Recurse -Force
+		Remove-Item $file.PSPath -Recurse -Force
 	}
 
 }
