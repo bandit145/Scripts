2b8ca0e924db9f4c58627894d3829aeaf2985c59
Phil <pgbson@gmail.com>
2016-10-20 09:45:51 -0400

    filedelete.ps1 working

--- filedelete.ps1
+++ filedelete.ps1
@@ -1,12 +1,15 @@
 #Written by Philip Bove
 #9/28/2016
 #
-$files = Get-ChildItem E:/downloads #Put path to folder you would like to run against
+$files = Get-ChildItem folder #Put path to folder you would like to run against
 $date = Get-Date
-$date = $date.AddDays(-1)
+$date = $date.AddDays(-1)#amount of days wanted to keep
 foreach($file in $files){
 	$fileinfo = Get-Item $file.PSPath
-	if($file.CreationTime -le $date){
+	if($file.Name -eq 'folder to not delete'){
+		Write-Host "Folder not Deleted"
+	}
+	Elseif($file.CreationTime -le $date){
 		Remove-Item $file.PSPath -Recurse -Force
 	}
 
