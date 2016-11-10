573872a71607c6721604c0f031cb4038ebd800b3
bandit145 <pgbson@gmail.com>
2016-11-10 00:19:17 -0500

    domain-prmote-permissions still wip

--- domain-promote-permission.ps1
+++ domain-promote-permission.ps1
@@ -1,46 +1,60 @@
 #Powershell implementation of DomJoinPermissions.CMD, Original CMD by Paul Williams, msresource.net http://bit.ly/2flkiRy
+#written 11/9/2016
 param(
     [Parameter(Mandatory = $true)]
-    [string]$groupname
+    [string]$groupname,
     [Parameter(Mandatory = $true)]
     [string]$dn
     )
-
 $compcont = "CN=Computers,"
 $namingcontexts = Get-ADRootDSE
-$namingcontexts = $namingcontexts.namingContexts
+$namingcontexts = $namingcontexts.namingContexts 
+function Error-Check{
+    param([string]$out)
+    if($out -like "*The command failed to complete successfully*"){
+        Write-Host $out
+        exit
+    }
+    "Hello "+$out
+}
 
 Write-Host "Configuring replication extended rights..."
 #give permissions to all naming contexts
 foreach($context in $namingcontexts){
-    dsacls $context /G $groupname:CA;"Add/Remove Replica In Domain";
-    dsacls $context /G $groupname:CA;"Replicating Directory Changes";
-    dsacls $context /G $groupname:CA;"Manage Replication Topology";
-    dsacls $context /G $groupname:CA;"Replicating Directory Changes All";
-    dsacls $context /G $groupname:CA;"Monitor Active Directory Replication";
+    $cmd  = -join('dsacls ',$context,' /G ',$groupname,':CA;"Add/Remove Replica In Domain";')
+    $out = Invoke-Expression $cmd
+    Error-Check -out $out
+    #replace all this
+    $cmd = sacls $context '/G' $groupname':CA;"Replicating Directory Changes";'
+    dsacls $context '/G' $groupname':CA;"Manage Replication Topology";'
+    dsacls $context '/G' $groupname':CA;"Replicating Directory Changes All";'
+    dsacls $context '/G' $groupname':CA;"Monitor Active Directory Replication";'
 }
 
 Write-Host "Configuring permissions on the sites container..."
-
-dsacls "CN=Sites, CN=Configuration,"$dn /G $groupname:CC;server; /I:S
-dsacls "CN=Sites, CN=Configuration,"$dn /G $groupname:CC;nTDSDSA; /I:S
-dsacls "CN=Sites, CN=Configuration,"$dn /G $groupname:WD;;nTDSDSA /I:S
-dsacls "CN=Sites, CN=Configuration,"$dn /G $groupname:CC;nTDSConnection; /I:S
+$string = -join('"CN=Sites, CN=Configuration,',$dn)
+dsacls $string' /G '$groupname':CC;server; /I:S'
+dsacls $string '/G' $groupname':CC;nTDSDSA; /I:S'
+dsacls $string '/G' $groupname':WD;;nTDSDSA /I:S'
+dsacls $string '/G' $groupname':CC;nTDSConnection; /I:S'
 
 Write-Host "Configuring permissions on the source (computers or staging, etc.) container..."
-
-dsacls -join($compcont,$dn) /G $groupname:WP;cn;computer /I:S
-dsacls -join($compcont,$dn) /G $groupname:WP;name;computer /I:S
-dsacls -join($compcont,$dn) /G $groupname:WP;distinguishedName;computer /I:S
-dsacls -join($compcont,$dn) /G $groupname:WP;servicePrincipalName;computer /I:S
-dsacls -join($compcont,$dn) /G $groupname:WP;serverReference;computer /I:S
-dsacls -join($compcont,$dn) /G $groupname:WP;userAccountControl;computer /I:S
-dsacls -join($compcont,$dn) /G $groupname:DC;computer;
+$string = -join($compcont,$dn)
+dsacls $string '/G' $groupname':WP;cn;computer /I:S'
+dsacls $string '/G' $groupname':WP;name;computer /I:S'
+dsacls $string '/G' $groupname':WP;distinguishedName;computer /I:S'
+dsacls $string '/G' $groupname':WP;servicePrincipalName;computer /I:S'
+dsacls $string '/G' $groupname':WP;serverReference;computer /I:S'
+dsacls $string '/G' $groupname':WP;userAccountControl;computer /I:S'
+dsacls $string '/G' $groupname':DC;computer;'
 
 Write-Host "Configuring permissions on the domain controllers OU..."
 
-dsacls "OU=Domain Controllers,"$dn /G $groupname:CC;computer;
+$string = -join("OU=Domain Controllers,",$dn)
+dsacls $string '/G' $groupname':CC;computer;'
+Write-Host "Granting WP for the msDS-NC-Replica-Locations attribute of the discoverer partition..."
 
-Write-Host "Granting WP for the msDS-NC-Replica-Locations attribute of the discoverer partition"
+$string = -join("CN=Partitions, CN=Configuration,",$dn)
+dsacls $string '/G' $groupname':WP;msDS-NC-Replica-Locations;'
 
-dsacls -join("CN=Partitions, CN=Configuration,",$dn) /G $groupname:WP;msDS-NC-Replica-Locations;
+Write-Host "Success!"
