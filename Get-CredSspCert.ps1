08435665fbf420aaa3c9ba8e7f11ea697dab9b2f
bandit145 <pgbson@gmail.com>
2016-12-02 00:35:39 -0500

    working on cmdlt to get credential thumb for credssp

---
+++ Get-CredSspCert.ps1
@@ -0,0 +1,2 @@
+#grab thumbprint of self signed cert for credssp
+Cert:\LocalMachine\Remote Desktop\
--- dnsreg.ps1
+++ dnsreg.ps1
@@ -1,8 +1,11 @@
 #Pulls data from sensu and updates DNS/DHCP entries
 $dnsserver = "DC1.meme.com"
 $dhcpserver = "DC1.meme.com"
+#get absolute path of script for the server.txt file
+$path = Get-Location
+$path = -join($path.path,"\server.txt")
 $data = Invoke-RestMethod -Method GET -uri http://sensu.meme.com:4567/clients
-$existing_servers = Get-Content -Path server.txt
+$existing_servers = Get-Content -Path $path
 $Newinv = New-Object System.Collections.ArrayList
 foreach($existing_server in $existing_servers){
 	$Newinv.Add($existing_server)
@@ -13,20 +16,18 @@ foreach($name in $data.name){
 		$Newinv.Add($name)
 		$address = Invoke-RestMethod -Method GET -uri http://sensu.meme.com:4567/clients/$name
 		Get-DhcpServerv4Lease -IPAddress $address.address -ComputerName $dhcpserver | Add-DhcpServerv4Reservation -ComputerName $dhcpserver
-		Add-DnsServerResourceRecordA -ZoneName meme.com -name $name -IPv4Address $address.address -ComputerName $dnsserver
+		Add-DnsServerResourceRecordA -ZoneName meme.com -Name $name -IPv4Address $address.address -ComputerName $dnsserver
 	}
 }
 foreach($name in $existing_servers){
 	if($name -NotIn $data.name){
 		$Newinv.Remove($name)
 		$ipaddr = Get-DNSServerResourceRecord -ZoneName meme.com -Name $name -ComputerName $dnsserver
-		Remove-DnsServerResourceRecord -ZoneName meme.com -Name $name -ComputerName $dnsserver
-		Remove-DhcpServerv4Reservation -ScopeID 192.168.1.0 -IPAddress $ipaddr.RecordData.IPv4Address.IPAddressToString -ComputerName $dhcpserver
+		Remove-DnsServerResourceRecord -ZoneName meme.com -Name $name -ComputerName $dnsserver -RRType "A" -Force
+		Remove-DhcpServerv4Reservation -IPAddress $ipaddr.RecordData.IPv4Address.IPAddressToString -ComputerName $dhcpserver
 	}
 
 
 
 }
-$Newinv | Out-File server.txt
-
-
+$Newinv | Out-File $path
